e59615a63dc07325e506631badab49c89ba462af
Anna-Grigencha <grigencha10@gmail.com>
2021-10-18 23:38:44 +0300

    feat: add queue

--- src/st-stack.js
+++ src/st-stack.js
@@ -12,34 +12,20 @@ const { NotImplementedError } = require("../extensions/index.js");
  * stack.pop(); // undefined
  *
  */
-//module.exports = class Stack {
-// push(/* element */) {
-//  throw new NotImplementedError('Not implemented');
-// remove line with error and write your code here
-//}
-
-//pop() {
-//throw new NotImplementedError('Not implemented');
-// remove line with error and write your code here
-//}
-
-// peek() {
-//  throw new NotImplementedError('Not implemented');
-// remove line with error and write your code here
-// }
-//}
-
-class Stack {
+module.exports = class Stack {
   constructor() {
     this.stack = [];
   }
-  push(item) {
-    this.stack.push(item);
+
+  push(element) {
+    this.stack.push(element);
   }
-  peek() {
-    return this.stack[this.array.length - 1];
-  }
+
   pop() {
     return this.stack.pop();
   }
-}
+
+  peek() {
+    return this.stack[this.stack.length - 1];
+  }
+};
